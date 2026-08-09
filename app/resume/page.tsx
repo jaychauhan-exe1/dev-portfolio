bea27d34328c9c3723476fe70cfda7b15a839b3f
jaychauhan-exe1 <temp.jaysingh@gmail.com>
2026-08-09 08:47:26 +0530

    feat: add resume page with download link to PDF file

---
+++ app/resume/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { GradientButton } from '@/components/ui/GradientButton'
+
+const Resume = () => {
+    return (
+        <a href="https://drive.google.com/file/d/1-2KCiw7-nTPk1E5LzBX_rtLjdseAhMD2/view?usp=sharing" download>
+            <GradientButton className=''>Download</GradientButton>
+        </a>
+    )
+}
+
+export default Resume     
